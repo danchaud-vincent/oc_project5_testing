2226fe2130360fdb66daf5e3cad0f92bbfc61591
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-14 11:14:49 +0200

    create a unit test suite with mock for MeComponent

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -1,49 +1,102 @@
 import { HttpClientModule } from '@angular/common/http';
-import {
-  ComponentFixture,
-  TestBed,
-  fakeAsync,
-  tick,
-} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from '../../services/session.service';
 
 import { MeComponent } from './me.component';
+import { first, of } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
 
+  const mockAdmin = {
+    id: 1,
+    email: 'admin@email.com',
+    lastName: 'test',
+    firstName: 'admin',
+    admin: true,
+    password: 'admin',
+    createdAt: new Date('2025-01-01'),
+    updatedAt: new Date('2025-01-01'),
+  };
+
+  const mockUser = {
+    id: 2,
+    email: 'user@email.com',
+    lastName: 'test',
+    firstName: 'user',
+    admin: false,
+    password: 'user',
+    createdAt: new Date('2025-01-01'),
+    updatedAt: new Date('2025-01-01'),
+  };
+
   const mockSessionService = {
     sessionInformation: {
-      admin: true,
+      token: 'Bearer',
+      type: 'token',
       id: 1,
+      username: 'james_t',
+      firstName: 'james',
+      lastName: 'test',
+      admin: true,
     },
+    logOut: jest.fn(),
   };
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MeComponent],
-      imports: [
-        MatSnackBarModule,
-        HttpClientModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-      ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
-    }).compileComponents();
 
-    fixture = TestBed.createComponent(MeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  const mockUserService = {
+    getById: jest.fn(),
+    delete: jest.fn().mockReturnValue(of(true)),
+  };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  const mockRouter = {
+    navigate: jest.fn(),
+  };
+
+  const mockMatSnackBar = {
+    open: jest.fn(),
+  };
+
+  describe('Unit Test Suite', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [MeComponent],
+        imports: [
+          MatSnackBarModule,
+          HttpClientModule,
+          MatCardModule,
+          MatFormFieldModule,
+          MatIconModule,
+          MatInputModule,
+        ],
+        providers: [
+          { provide: UserService, useValue: mockUserService },
+          { provide: SessionService, useValue: mockSessionService },
+          { provide: Router, useValue: mockRouter },
+          { provide: MatSnackBar, useValue: mockMatSnackBar },
+        ],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(MeComponent);
+      component = fixture.componentInstance;
+    });
+
+    it('should create', () => {
+      // ARRANGE
+      mockUserService.getById.mockReturnValue(of(mockAdmin));
+
+      // ACT
+      fixture.detectChanges();
+
+      // ASSERT
+      expect(component).toBeTruthy();
+    });
   });
 });
