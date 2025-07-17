be8e110fa646e3c83a371b6e47af8169264230ab
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-17 12:56:03 +0200

    add integration to login and logout in AppComponent

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,5 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
@@ -8,11 +13,27 @@ import { AppComponent } from './app.component';
 import { of } from 'rxjs';
 import { SessionService } from './services/session.service';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { getByDataTest } from 'src/test-utils/test-utils';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
   let router: Router;
+  let location: Location;
+  let sessionService: SessionService;
+
+  const testRoutes = [{ path: '', component: AppComponent }];
+
+  const mockSessionInformation = {
+    token: 'Bearer',
+    type: 'token',
+    id: 1,
+    username: 'admin',
+    firstName: 'admin',
+    lastName: 'test',
+    admin: true,
+  };
 
   describe('Unit Test suite', () => {
     const mockSessionService = {
@@ -46,7 +67,7 @@ describe('AppComponent', () => {
       expect(mockSessionService.$isLogged).toHaveBeenCalled();
     });
 
-    it('should call sessionService.logOut and navigate to', () => {
+    it('should call sessionService.logOut and navigate to Home page', () => {
       // ARRANGE
       const spyNavigate = jest.spyOn(router, 'navigate');
 
@@ -57,5 +78,94 @@ describe('AppComponent', () => {
       expect(mockSessionService.logOut).toHaveBeenCalled();
       expect(spyNavigate).toHaveBeenCalledWith(['']);
     });
+  });
+
+  describe('Integration Test Suite', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule.withRoutes(testRoutes),
+          HttpClientModule,
+          MatToolbarModule,
+        ],
+        providers: [SessionService],
+        declarations: [AppComponent],
+      }).compileComponents();
+
+      // INJECT
+      router = TestBed.inject(Router);
+      location = TestBed.inject(Location);
+      sessionService = TestBed.inject(SessionService);
+
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.componentInstance;
+    });
+
+    it('should display logOut button if logged, and not display register menu', () => {
+      // ARRANGE
+      jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(true));
+
+      // ACT
+      fixture.detectChanges();
+
+      // GET HTML ELEMENTS
+      const logoutBtn = getByDataTest(
+        fixture,
+        'logout-btn'
+      ) as HTMLButtonElement;
+      const registerMenuEl = getByDataTest(fixture, 'notLogged') as HTMLElement;
+
+      // ASSERT
+      expect(logoutBtn).not.toBeNull();
+      expect(registerMenuEl).toBeNull();
+    });
+
+    it('should not display logOut button if not logged, and display register menu', () => {
+      // ARRANGE
+      jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(false));
+
+      // ACT
+      fixture.detectChanges();
+
+      // GET HTML ELEMENTS
+      const logoutBtn = getByDataTest(
+        fixture,
+        'logout-btn'
+      ) as HTMLButtonElement;
+      const registerMenuEl = getByDataTest(fixture, 'notLogged') as HTMLElement;
+
+      // ASSERT
+      expect(logoutBtn).toBeNull();
+      expect(registerMenuEl).not.toBeNull();
+    });
+
+    it('should logOut the user, navigate to Home Page and hide the register menu when logOut button is clicked', fakeAsync(() => {
+      // ARRANGE
+      sessionService.logIn(mockSessionInformation);
+
+      fixture.detectChanges();
+      tick();
+
+      // GET HTML ELEMENTS
+      const logoutBtn = getByDataTest(
+        fixture,
+        'logout-btn'
+      ) as HTMLButtonElement;
+
+      // ACT
+      logoutBtn.click();
+      fixture.detectChanges();
+      tick();
+
+      // ASSERT
+      const registerMenuEl = getByDataTest(fixture, 'notLogged') as HTMLElement;
+      expect(registerMenuEl).not.toBeNull();
+
+      const loggedMenuEl = getByDataTest(fixture, 'isLogged');
+      expect(loggedMenuEl).toBeNull();
+
+      // ASSERT LOCATION
+      expect(location.path()).toBe('/');
+    }));
   });
 });
