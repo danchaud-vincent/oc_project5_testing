2fef4af65e5dddd2c95ab6ccd12b914445ab600c
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-18 16:31:27 +0200

    add e2e test to test a successfull registration

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -1,1 +1,23 @@
-describe('Register spec', () => {});
+describe('Register spec', () => {
+  beforeEach(() => {
+    cy.visit('/register');
+  });
+
+  it('should register successfully', () => {
+    // ARRANGE: mock http request
+    cy.intercept('POST', 'api/auth/register', {
+      body: {},
+    });
+
+    // ACT : fill the form with valid inputs
+    cy.get('[data-test="firstName"]').type('username'); // length >= 3
+    cy.get('[data-test="lastName"]').type('userLastName'); // length >= 3
+    cy.get('[data-test="email"]').type('test@email.com'); //  format ...@...
+    cy.get('[data-test="password"]').type('password123'); // length >= 3
+    cy.get('[data-test="submit-btn"]').click();
+
+    // ASSERT
+    cy.location('pathname').should('equal', '/login');
+    cy.get('[data-test="login-form"]').should('be.visible');
+  });
+});
