1ef6f7b27654944f6dcf4459467105b4e0f4ea3b
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-21 10:28:51 +0200

    refactor login.cy tests with fixtures

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,39 +1,21 @@
-import { SessionInformation } from '../../src/app/interfaces/sessionInformation.interface';
+import { mockAdminSessionInfo } from 'cypress/fixtures/adminSessionInformation.fixtures';
+import { mockUserSessionInfo } from 'cypress/fixtures/userSessionInformation.fixtures';
 
 describe('Login spec', () => {
-  const mockAdminSessionInfo: SessionInformation = {
-    token: 'Bearer',
-    type: 'token',
-    id: 1,
-    username: 'admin@email.com',
-    firstName: 'admin',
-    lastName: 'admin',
-    admin: true,
-  };
-
-  const mockUserSessionInfo: SessionInformation = {
-    token: 'Bearer',
-    type: 'token',
-    id: 2,
-    username: 'user@email.com',
-    firstName: 'user',
-    lastName: 'user',
-    admin: false,
-  };
-
   beforeEach(() => {
     cy.visit('/login');
   });
 
   it('should return to login when trying to go to sessions (AuthGard)', () => {
+    // -------------- ACT --------------
     cy.visit('/sessions');
 
-    // ASSERT
+    // -------------- ASSERT --------------
     cy.location('pathname').should('equal', '/login');
   });
 
   it('should login successfully as an ADMIN', () => {
-    // ARRANGE: mock http request
+    /// -------------- ARRANGE --------------
     cy.intercept('POST', '/api/auth/login', {
       body: mockAdminSessionInfo,
     });
@@ -46,19 +28,19 @@ describe('Login spec', () => {
       []
     ).as('session');
 
-    // ACT: fill the form
+    // -------------- ACT --------------
     cy.get('input[formControlName=email]').type('yoga@studio.com');
     cy.get('input[formControlName=password]').type(
       `${'test!1234'}{enter}{enter}`
     );
 
-    // ASSERT: on sessions page
+    // -------------- ASSERT --------------
     cy.location('pathname').should('equal', '/sessions');
     cy.get('[data-test="rentals-title"]').should('be.visible');
   });
 
   it('should login successfully as an USER', () => {
-    // ARRANGE: mock http request
+    // -------------- ARRANGE --------------
     cy.intercept('POST', '/api/auth/login', {
       body: mockUserSessionInfo,
     });
@@ -71,19 +53,19 @@ describe('Login spec', () => {
       []
     ).as('session');
 
-    // ACT: fill the form
+    /// -------------- ACT --------------
     cy.get('input[formControlName=email]').type('yoga@studio.com');
     cy.get('input[formControlName=password]').type(
       `${'test!1234'}{enter}{enter}`
     );
 
-    // ASSERT: on sessions page
+    // -------------- ASSERT --------------
     cy.location('pathname').should('equal', '/sessions');
     cy.get('[data-test="create-btn"]').should('not.exist');
   });
 
   it('should detect an error when login failed', () => {
-    // ARRANGE: mock an auth error
+    /// -------------- ARRANGE --------------
     cy.intercept('POST', '/api/auth/login', {
       statusCode: 404,
       body: {
@@ -91,19 +73,19 @@ describe('Login spec', () => {
       },
     }).as('loginError');
 
-    // ACT: fill the form and submit
+    // -------------- ACT --------------
     cy.get('[data-test="email"]').type('unknown@email');
     cy.get('[data-test="password"]').type(`${'test!1234'}`);
     cy.get('[data-test="submit-btn"]').click();
 
-    // ASSERT: expect error message
+    // -------------- ASSERT --------------
     cy.get('[data-test="error"]')
       .should('be.visible')
       .should('have.text', 'An error occurred');
   });
 
   it('should hide the submit button when login credentials dont respect the requirements', () => {
-    // ARRANGE: mock http request
+    // -------------- ARRANGE --------------
     cy.intercept('POST', '/api/auth/login', {
       body: mockAdminSessionInfo,
     });
@@ -116,11 +98,11 @@ describe('Login spec', () => {
       []
     ).as('session');
 
-    // ACT: fill the form and submit
+    // -------------- ACT --------------
     cy.get('[data-test="email"]').type('unknown@'); // wrong email format
     cy.get('[data-test="password"]').type(`${'tes'}`); // password length < 3
 
-    // ASSERT submit button disabled
+    // -------------- ASSERT --------------
     cy.get('[data-test="submit-btn"]').should('be.disabled');
   });
 });
