5a8c647d5b85d9e5fc1d688b35b9c18aa3a2ce3c
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-21 10:25:18 +0200

    refactor account.cy tests with the fixtures

--- front/cypress/e2e/account.cy.ts
+++ front/cypress/e2e/account.cy.ts
@@ -1,32 +1,15 @@
-import { SessionInformation } from '../../src/app/interfaces/sessionInformation.interface';
-import { User } from '../../src/app/interfaces/user.interface';
+import { user } from 'cypress/fixtures/user.fixtures';
+import { admin } from 'cypress/fixtures/admin.fixtures';
+import { mockUserSessionInfo } from 'cypress/fixtures/userSessionInformation.fixtures';
+import { mockAdminSessionInfo } from 'cypress/fixtures/adminSessionInformation.fixtures';
 
 describe('Me spec', () => {
   describe('ADMIN account', () => {
-    const mockAdminSessionInfo: SessionInformation = {
-      token: 'Bearer',
-      type: 'token',
-      id: 1,
-      username: 'admin@email.com',
-      firstName: 'admin',
-      lastName: 'admin',
-      admin: true,
-    };
-
-    const admin: User = {
-      id: 1,
-      email: 'admin@email.com',
-      lastName: 'admin',
-      firstName: 'admin',
-      admin: true,
-      password: 'admin',
-      createdAt: new Date('2025-01-01'),
-      updatedAt: new Date('2025-01-01'),
-    };
-
     beforeEach(() => {
+      // visit login page
       cy.visit('/login');
 
+      // Mock http requests
       cy.intercept('POST', '/api/auth/login', {
         body: mockAdminSessionInfo,
       });
@@ -39,62 +22,42 @@ describe('Me spec', () => {
         []
       ).as('session');
 
-      // ACT: fill the login form and submit
-      cy.get('[data-test="email"]').type(admin.email);
-      cy.get('[data-test="password"]').type(admin.password);
-      cy.get('[data-test="submit-btn"]').click();
-
-      // ARRANGE : mock http request user
       cy.intercept('GET', `api/user/${admin.id}`, {
         body: admin,
       });
+
+      // fill the login form and submit
+      cy.get('[data-test="email"]').type(admin.email);
+      cy.get('[data-test="password"]').type(admin.password);
+      cy.get('[data-test="submit-btn"]').click();
 
       // Visit the account
       cy.get('[data-test="account"]').click();
     });
 
-    it('should display show admin specific data', () => {
-      // ASSERT
+    it('should display admin specific data', () => {
+      // -------------- ASSERT --------------
       cy.get('[data-test="account-title"]').should('be.visible');
       cy.get('[data-test="delete-btn"]').should('not.exist');
       cy.get('[data-test="admin"]').should('be.visible');
     });
 
     it('should navigate to sessions page when back is clicked', () => {
-      // ACT
+      // -------------- ARRANGE --------------
       cy.get('[data-test="back-btn"]').should('be.visible').click();
 
-      // ASSERT
+      // -------------- ASSERT --------------
       cy.location('pathname').should('equal', '/sessions');
       cy.get('[data-test="rentals-title"]').should('be.visible');
     });
   });
 
   describe('USER account', () => {
-    const mockUserSessionInfo: SessionInformation = {
-      token: 'Bearer',
-      type: 'token',
-      id: 2,
-      username: 'user@email.com',
-      firstName: 'user',
-      lastName: 'user',
-      admin: false,
-    };
-
-    const user: User = {
-      id: 2,
-      email: 'user@email.com',
-      lastName: 'user',
-      firstName: 'user',
-      admin: false,
-      password: 'user',
-      createdAt: new Date('2025-01-01'),
-      updatedAt: new Date('2025-01-01'),
-    };
-
     beforeEach(() => {
+      // visit login page
       cy.visit('/login');
 
+      // mock http requests
       cy.intercept('POST', '/api/auth/login', {
         body: mockUserSessionInfo,
       });
@@ -107,29 +70,29 @@ describe('Me spec', () => {
         []
       ).as('session');
 
-      // ACT: fill the login form and submit
-      cy.get('[data-test="email"]').type(user.email);
-      cy.get('[data-test="password"]').type(user.password);
-      cy.get('[data-test="submit-btn"]').click();
-
-      // ARRANGE : mock http request user
       cy.intercept('GET', `api/user/${user.id}`, {
         body: user,
       });
+
+      // -------------- ACT --------------
+      // fill the login form and submit
+      cy.get('[data-test="email"]').type(user.email);
+      cy.get('[data-test="password"]').type(user.password);
+      cy.get('[data-test="submit-btn"]').click();
 
       // Visit the account
       cy.get('[data-test="account"]').click();
     });
 
     it('should display user specific data and delete button', () => {
-      // ASSERT
+      // -------------- ASSERT --------------
       cy.get('[data-test="account-title"]').should('be.visible');
       cy.get('[data-test="delete-btn"]').should('exist');
       cy.get('[data-test="admin"]').should('not.exist');
     });
 
     it('should delete a user account and navigate to Home page', () => {
-      // ARRANGE
+      // -------------- ARRANGE --------------
       cy.intercept(
         {
           method: 'delete',
@@ -138,10 +101,10 @@ describe('Me spec', () => {
         []
       );
 
-      // ACT
+      // -------------- ACT --------------
       cy.get('[data-test="delete-btn"]').click();
 
-      // ASSERT
+      // -------------- ASSERT --------------
       cy.contains('Your account has been deleted !').should('be.visible');
       cy.location('pathname').should('equal', '/');
     });
