844265c6dc92d3e8f610b7535c7c2e75e63acdf9
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-25 17:47:09 +0200

    add Activeprofile to the test

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -2,8 +2,10 @@ package com.openclassrooms.starterjwt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("unit-test")
 public class SpringBootSecurityJwtApplicationTests {
 
 	@Test
