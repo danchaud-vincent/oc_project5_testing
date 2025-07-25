652388c3649ea6bf0851eeef83e61b9fa29a83d2
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-25 17:49:45 +0200

    change activeprofile name, add @tag and refactor unit tests

--- back/src/test/java/com/openclassrooms/starterjwt/unit/repository/UserRepositoryTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/unit/repository/UserRepositoryTests.java
@@ -3,7 +3,9 @@ package com.openclassrooms.starterjwt.unit.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -14,7 +16,8 @@ import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 
 @DataJpaTest
-@ActiveProfiles("jpa-test")
+@ActiveProfiles("unit-test")
+@Tag("unit")
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class UserRepositoryTests {
 
