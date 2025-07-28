d66b52c30168f48eaa387d1de8b9e7cf3f9052f0
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-28 15:31:31 +0200

    add unit tests for UserService

--- back/src/test/java/com/openclassrooms/starterjwt/unit/services/UserServiceTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/unit/services/UserServiceTests.java
@@ -1,10 +1,21 @@
 package com.openclassrooms.starterjwt.unit.services;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 import com.openclassrooms.starterjwt.services.UserService;
 
@@ -16,5 +27,44 @@ public class UserServiceTests {
 
     @InjectMocks
     private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        // ARRANGE
+        user = User.builder()
+                .id(1L)
+                .email("user@email.com")
+                .lastName("lastName")
+                .firstName("firstName")
+                .password("1234")
+                .admin(false)
+                .build();
+    }
+
+    @Test
+    public void UserService_delete_ShouldDeleteUser() {
+        // ARANGE
+        doNothing().when(userRepository).deleteById(user.getId());
+
+        // ACT
+        userService.delete(user.getId());
+
+        // ASSERT
+        verify(userRepository, times(1)).deleteById(user.getId());
+    }
+
+    @Test
+    public void UserService_findById_ShouldReturnAUser() {
+        // ARRANGE
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        // ACT
+        userService.findById(user.getId());
+
+        // ASSERT
+        verify(userRepository).findById(user.getId());
+    }
 
 }
