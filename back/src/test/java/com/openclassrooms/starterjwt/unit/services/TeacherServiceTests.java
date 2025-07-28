900ad6c9fa519e755b060562b9e19f421f1bfcb2
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-28 15:49:17 +0200

    add unit tests for TeacherService

---
+++ back/src/test/java/com/openclassrooms/starterjwt/unit/services/TeacherServiceTests.java
@@ -0,0 +1,83 @@
+package com.openclassrooms.starterjwt.unit.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+import com.openclassrooms.starterjwt.services.TeacherService;
+
+@ExtendWith(MockitoExtension.class)
+public class TeacherServiceTests {
+
+    @Mock
+    private TeacherRepository teacherRepository;
+
+    @InjectMocks
+    private TeacherService teacherService;
+
+    private List<Teacher> teachers;
+    private Teacher teacher;
+
+    @BeforeEach
+    public void init() {
+        // create a list of teachers
+        Teacher teacher1 = Teacher.builder()
+                .lastName("lastNameTeacher")
+                .firstName("firstNameTeacher")
+                .build();
+
+        Teacher teacher2 = Teacher.builder()
+                .lastName("lastNameTeacher")
+                .firstName("firstNameTeacher")
+                .build();
+
+        teachers = List.of(teacher1, teacher2);
+
+        // create a teacher
+        teacher = Teacher.builder()
+                .id(1L)
+                .lastName("lastNameTeacher")
+                .firstName("firstNameTeacher")
+                .build();
+    }
+
+    @Test
+    public void TeacherService_findAll_ShouldReturnAListOfTeachers() {
+        // ARRANGE
+        when(teacherRepository.findAll()).thenReturn(teachers);
+
+        // ACT
+        List<Teacher> teachersReturn = teacherService.findAll();
+
+        // ASSERT
+        verify(teacherRepository).findAll();
+        assertThat(teachersReturn.size()).isEqualTo(teachers.size());
+    }
+
+    @Test
+    public void TeacherService_findById_ShouldReturnATeacher() {
+        // ARRANGE
+        when(teacherRepository.findById(anyLong())).thenReturn(Optional.of(teacher));
+
+        // ACT
+        Teacher teacherFound = teacherService.findById(teacher.getId());
+
+        // ASSERT
+        verify(teacherRepository).findById(teacher.getId());
+        assertThat(teacherFound.getLastName()).isEqualTo(teacher.getLastName());
+    }
+
+}
