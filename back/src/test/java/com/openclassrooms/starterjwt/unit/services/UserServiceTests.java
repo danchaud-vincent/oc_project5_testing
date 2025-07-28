c7926276ffb6538c7f0f25ad04b48c16ea03cf60
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-28 11:47:59 +0200

    create a service folder in unit tests folder, and create a UserServiceTests file

---
+++ back/src/test/java/com/openclassrooms/starterjwt/unit/services/UserServiceTests.java
@@ -0,0 +1,20 @@
+package com.openclassrooms.starterjwt.unit.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.UserService;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+}
