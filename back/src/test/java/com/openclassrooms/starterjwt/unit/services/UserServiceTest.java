af183f0f751227e1208b762570d09253565bc9ca
Vincent Danchaud <vdanchaud@gmail.com>
2025-07-30 17:11:12 +0200

    clean code

--- back/src/test/java/com/openclassrooms/starterjwt/unit/services/UserServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/unit/services/UserServiceTest.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.starterjwt.unit.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
