ed7828f815fb5acbfc7cd38a93c77af4e60041e3
Vincent Danchaud <vdanchaud@gmail.com>
2025-08-01 18:25:12 +0200

    add integration test for delete in UserControllerTest

--- back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/UserControllersTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/UserControllersTest.java
@@ -1,8 +1,6 @@
 package com.openclassrooms.starterjwt.integration.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,14 +14,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.dto.UserDto;
@@ -34,13 +29,11 @@ import com.openclassrooms.starterjwt.repository.UserRepository;
 import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
 import com.openclassrooms.starterjwt.services.UserService;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
-@Tag("integration")
+@Tag("controller")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllersTest extends BaseIntegrationTest {
 
@@ -70,6 +63,16 @@ public class UserControllersTest extends BaseIntegrationTest {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private User user;
+    private String token;
+
+    @BeforeEach
+    public void init() {
+        Map<String, Object> authenticateUserMap = createAndAuthenticateTestUser();
+        user = (User) authenticateUserMap.get("user");
+        token = (String) authenticateUserMap.get("token");
+    }
 
     @AfterEach
     public void clean() {
@@ -108,9 +111,6 @@ public class UserControllersTest extends BaseIntegrationTest {
     @Test
     public void findById_shouldReturn401_whenRequestWithoutToken() throws Exception {
         // ARRANGE
-        Map<String, Object> authenticateUserMap = createAndAuthenticateTestUser();
-        User user = (User) authenticateUserMap.get("user");
-
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "");
         HttpEntity<Void> entity = new HttpEntity<>(headers);
@@ -129,11 +129,6 @@ public class UserControllersTest extends BaseIntegrationTest {
     @Test
     public void findById_shouldReturn200AndUSerDto_whenRequestWithValidToken() {
         // ARRANGE
-        Map<String, Object> authenticateUserMap = createAndAuthenticateTestUser();
-        User user = (User) authenticateUserMap.get("user");
-        String token = (String) authenticateUserMap.get("token");
-
-        // set headers for http request
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
         HttpEntity<Void> entity = new HttpEntity<>(headers);
@@ -150,6 +145,143 @@ public class UserControllersTest extends BaseIntegrationTest {
         // ASSERT
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseUserDto.getEmail()).isEqualTo(user.getEmail());
+        assertThat(responseUserDto.getFirstName()).isEqualTo(user.getFirstName());
+        assertThat(responseUserDto.getLastName()).isEqualTo(user.getLastName());
+        assertThat(responseUserDto.isAdmin()).isEqualTo(user.isAdmin());
     }
 
+    @Test
+    public void findById_shouldReturn404_whenUserIdNotInDatabase() {
+        // ARRANGE
+        Long userIdNotInDB = 999L;
+
+        // set headers for http request
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        // ACT
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/api/user/" + userIdNotInDB,
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        // ASSERT
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void findById_shouldReturn400_whenRequestWithInvalidID() {
+        // ARRANGE
+        String invalidID = "abc";
+
+        // set headers for http request
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        // ACT
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/api/user/" + invalidID,
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        // ASSERT
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void delete_shouldReturn200_whenUserIsDeleted() {
+        // set headers for http request
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        // ACT
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/api/user/" + user.getId(),
+                HttpMethod.DELETE,
+                entity,
+                String.class);
+
+        // ASSERT
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(userRepository.findAll().size()).isEqualTo(0);
+    }
+
+    @Test
+    public void delete_shouldReturn404_whenUserNotFound() {
+        // ARRANGE
+        Long userIdNotInDB = 999L;
+
+        // set headers for http request
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        // ACT
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/api/user/" + userIdNotInDB,
+                HttpMethod.DELETE,
+                entity,
+                String.class);
+
+        // ASSERT
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(userRepository.findAll().size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void delete_shouldReturn400_whenIdNotValid() {
+        // ARRANGE
+        String idInvalid = "abc";
+
+        // set headers for http request
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        // ACT
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/api/user/" + idInvalid,
+                HttpMethod.DELETE,
+                entity,
+                String.class);
+
+        // ASSERT
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void delete_shouldReturn401_whenAUserWantToDeleteAnotherUser() {
+        // ARRANGE: add another user in db
+        User otherUser = User.builder()
+                .email("otherUser@email.com")
+                .lastName("otherUserLastName")
+                .firstName("otherUserFirstName")
+                .password(bCryptPasswordEncoder.encode("otherPassword"))
+                .admin(true)
+                .build();
+
+        userRepository.saveAndFlush(otherUser);
+        Optional<User> otherUserDB = userRepository.findByEmail(otherUser.getEmail());
+
+        // set headers for http request
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        // ACT : try to delete the otherUser with the token of the user already
+        // connected
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/api/user/" + otherUserDB.get().getId(),
+                HttpMethod.DELETE,
+                entity,
+                String.class);
+
+        // ASSERT
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
 }
